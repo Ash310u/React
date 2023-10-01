fed539f7a9ebda6b1aaf09cb381f0dfeb3c01ecf
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-01 11:17:55 +0530

    Commited a custom react V2

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,9 +1,17 @@
 function customRender(reactElement, container) {
+    // const domElemnet = document.createElement(reactElement.type)
+    // domElemnet.innerHTML = reactElement.children
+    // domElemnet.setAttribute('href', reactElement.props.href)
+    // domElemnet.setAttribute('target', reactElement.props.target)
+
+    // container.appendChild(domElemnet)
+
     const domElemnet = document.createElement(reactElement.type)
     domElemnet.innerHTML = reactElement.children
-    domElemnet.setAttribute('href', reactElement.props.href)
-    domElemnet.setAttribute('target', reactElement.props.target)
-
+    Object.keys(reactElement.props).forEach((prop) => {
+        domElemnet.setAttribute(prop, reactElement.props[prop])
+    })
+    
     container.appendChild(domElemnet)
 }
 
