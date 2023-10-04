023ef0323e982c1f8cae7878226d5961f01e5be2
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-04 22:59:02 +0530

    Commited background color changer using tailwind css and useState method

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('#272727')
 
   return (
-    <>
-      
-    </>
+    <div className='w-screen h-screen duration-200 '
+      style={{ backgroundColor: color }}
+    >
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-16 shadow-lg  px-3 py-2  bg-gray-200 rounded-3xl'>
+          <button onClick={() => setColor('red')} className='px-3 py-2 text-white rounded-3xl' style={{backgroundColor:'red'}}>Red</button>
+          <button onClick={() => setColor('green')} className='px-3 py-2 text-white rounded-3xl' style={{backgroundColor:'green'}}>green</button>
+          <button onClick={() => setColor('orange')} className='px-3 py-2 text-white rounded-3xl' style={{backgroundColor:'orange'}}>orange</button>
+          <button onClick={() => setColor('blue')} className='px-3 py-2 text-white rounded-3xl' style={{backgroundColor:'skyblue'}}>skyblue</button>
+          <button onClick={() => setColor('lavender')} className='px-3 py-2 text-white rounded-3xl' style={{backgroundColor:'purple'}}>purple</button>
+          <button onClick={() => setColor('olive')} className='px-3 py-2 text-white rounded-3xl' style={{backgroundColor:'olive'}}>olive</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
