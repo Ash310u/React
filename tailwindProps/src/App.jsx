ec131ec6416494970e40bc6ccad58d0677ace008
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-04 10:00:39 +0530

    Commited different names for different Cards using props in Card component

--- tailwindProps/src/App.jsx
+++ tailwindProps/src/App.jsx
@@ -3,16 +3,12 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-  let MyObj = {
-    userName:'Ashu',
-    age:19
-  }
-  let MyAry = [1,2,3]
 
   return (
     <>
       <h1 className='text-blue-400 font-bold p-4 bg-slate-100 rounded-xl mb-4'>Tailwind test</h1>
-      <Card whatever='nothing' someObj={MyObj} someAry={MyAry}/>
+      <Card username='Saima' />
+      <Card username='Riya' />
     </>
   )
 }
