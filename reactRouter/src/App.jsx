c569c21753728344801175cdfe0353fb389a8e55
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-12 10:30:49 +0530

    Removed some unwanted code

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,13 +1,8 @@
 import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold text-gray-300">
-        Hello world!
-      </h1>
     </>
   )
 }
