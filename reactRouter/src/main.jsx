55a44e09614878a2cce130254db302783cdbb938
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-12 23:23:59 +0530

    Commited a new syntax for the router element using 'createRoutesFromElements'

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,30 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
 import './index.css'
 import Layout from './Layout'
 import { Home, Contact, About} from './components/index'
 
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<Layout />,
-    children:[
-      {
-        path:'',
-        element:<Home />
-      },
-      {
-        path:'about',
-        element:<About/>
-      },
-      {
-        path:'contact',
-        element:<Contact/>
-      }
-    ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path:'/',
+//     element:<Layout />,
+//     children:[
+//       {
+//         path:'',
+//         element:<Home />
+//       },
+//       {
+//         path:'about',
+//         element:<About/>
+//       },
+//       {
+//         path:'contact',
+//         element:<Contact/>
+//       }
+//     ]
+//   }
+// ])
+
+// A different syntax to create a router -->
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+        <Route path='' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+      </Route>
+    )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
