0ec1bbfa8937f0a4dcf29b7aa0052cc042fa4c32
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-13 17:44:21 +0530

    Commited a Github component

---
+++ reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Github() {
+    return (
+        <>
+            <div className='text-center text-3xl m-4 p-4 bg-gray-600 text-white'>
+                Github
+            </div>
+        </>
+    )
+}
