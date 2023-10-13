10a0d1d5de781d5d79eb738cc1e3ebb28c4fe78d
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-13 17:44:52 +0530

    Commited a path for Github component

--- reactRouter/src/components/index.js
+++ reactRouter/src/components/index.js
@@ -4,5 +4,6 @@ import Footer from './Footer/Footer';
 import About from './About/About'
 import Contact from './Contact/Contact';
 import User from './User/User';
+import Github from './Github/Github';
 
-export { Header, Home, Footer, Contact, About, User }
+export { Header, Home, Footer, Contact, About, User, Github }
