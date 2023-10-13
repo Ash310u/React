9fbe261e1f3c3da6f85a0f062eb3a79f289609e3
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-13 18:03:52 +0530

    Commited some github stats and fetch github Api to show follower on github

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Github() {
+    const [ data, setData ] = useState([])
+    useEffect(() => {
+        fetch('https://api.github.com/users/ash310u')
+            .then(res => res.json())
+            .then(data => setData(data))
+    })
     return (
         <>
             <div className='text-center text-3xl m-4 p-4 bg-gray-600 text-white'>
-                Github
+                Github followers: {data.followers}
+            </div>
+            <div className="flex justify-center flex-row p-4">
+                <img src="https://github-readme-streak-stats.herokuapp.com/?user=ash310u" alt="" className="m-4" />
+                <img src="https://github-readme-stats-anuraghazra1.vercel.app/api/top-langs/?username=ash310u" alt="" className="m-4" />
+                <img src="https://github-readme-stats.vercel.app/api?username=ash310u" alt="" className="m-4" />
             </div>
         </>
     )
