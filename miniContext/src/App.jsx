4a09fce0c19bb72ce4b1f82dbccadd8a87646c6b
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-15 17:04:38 +0530

    Commited  some changes in tailwind css

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -7,9 +7,11 @@ function App() {
 
   return (
     <UserContextProvider>
-      <h1>Context API</h1>
-      <Login/>
-      <Profile/>
+      <div className='bg-gray-700 flex  flex-col justify-center items-center m-8 p-4 text-white  rounded-xl'>
+        <h1>Context API</h1>
+        <Login/>
+        <Profile/>
+      </div>
     </UserContextProvider>
   )
 }
--- miniContext/src/components/Login.jsx
+++ miniContext/src/components/Login.jsx
@@ -12,11 +12,11 @@ function Login() {
         setUser({username, password})
     }
     return (
-        <div className='bg-gray-700 flex  flex-col justify-center items-center m-8 p-4   rounded-xl'>
-            <h2 className="text-gray-100 text-3xl justify-center">Login</h2>
-            <input type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} className="p-2 m-5 "/>
-            <input type="text" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} className="p-2 m-2"/>
-            <button onClick={handleSubmit}>Submit</button>
+        <div >
+            <h2 className="text-center text-3xl ">Login</h2>
+            <input type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} className="p-2 m-2 text-black"/>
+            <input type="text" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} className="p-2 m-2 text-black"/>
+            <button onClick={handleSubmit} className="bg-gray-400 p-2 rounded-xl">Submit</button>
         </div>
     )
 }
