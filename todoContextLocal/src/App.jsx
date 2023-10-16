45a2dc8f4e1a44b53b38310c48a00bc8c5a6a82c
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-16 14:45:13 +0530

    Commited the context api functions('addTodo, updateTodo, deleteTodo, toggleTodo') in App.js

--- todoContextLocal/src/App.jsx
+++ todoContextLocal/src/App.jsx
@@ -3,8 +3,33 @@ import { TodoProvider } from './contexts'
 import './App.css'
 
 function App() {
-
   const [todos, setTodos] = useState([])
+  
+  const addTodo = (todo) => {
+    setTodos((prev) => [...prev, { id:Date.now(), ...todo}] )
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => {
+      prev.map((prevTodo) => {
+        prevTodo.id === id ? todo : prevTodo;
+      })
+    })
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((prevTodo) => {
+      prevTodo.id !== id
+    }))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => {
+      prev.map((prevTodo) => {            // Here we overwrite the value of completed 
+        prevTodo.id === id ? {...prevTodo, completed:!prevTodo.completed} : prevTodo
+      })
+    })
+  }
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
