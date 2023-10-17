fbe8fe1495b473b6b92f73ddfc65e3222c72677b
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-17 17:12:44 +0530

    Commited TodoForm.jsx component with all the functional work using 'useTodo' hook and created a 'add' function for calling 'addTodo' inside

--- todoContextLocal/src/components/TodoForm.jsx
+++ todoContextLocal/src/components/TodoForm.jsx
@@ -1,11 +1,26 @@
-function TodoForm() {
+import React, { useState } from "react";
+import { useTodo } from "../contexts";
 
+
+function TodoForm() {
+    const [todo, setoTodo] = useState("")
+    const {addTodo} = useTodo
+
+    const add = (e) => {
+        e.preventDefault()
+        if(!todo) return 
+
+        addTodo({ todo, completed: false })
+        setoTodo("")
+    }
 
     return (
-        <form className="flex">
+        <form onSubmit={add} className="flex">
             <input
                 type="text"
                 placeholder="Write Todo..."
+                onChange={(e) => setoTodo(e.target.value) }
+                value={todo}
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
             />
             <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
