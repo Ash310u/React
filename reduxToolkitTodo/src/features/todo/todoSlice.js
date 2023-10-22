4ad797bf662364fb8005293dbe1c7c42e7d0300f
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-22 18:00:45 +0530

    Commited a comment related to Slice reducers

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -10,6 +10,7 @@ const initialState = {
 export const todoSlice = createSlice({
     name:'todos',
     initialState,
+    // A slice reducer is the reducer responsible for handling actions and updating the data for a given slice.
     reducers:{ //'state' it gives the access of current initialstate properties and values.
         addTodo: (state, action) => {},
         removeTodo:(state, action) => {}
