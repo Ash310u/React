80a2b226e855b11cd447a0728c94874d02af3273
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-23 15:33:33 +0530

    Commited addTodo function, added a todo using 'action.payload' and set a id using 'nanoid' method and push the new todo using 'state.todos.push()' method

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -12,7 +12,13 @@ export const todoSlice = createSlice({
     initialState,
     // A slice reducer is the reducer responsible for handling actions and updating the data for a given slice.
     reducers:{ //'state' it gives the access of current initialstate properties and values.
-        addTodo: (state, action) => {},
+        addTodo: (state, action) => {
+            const todo = {
+                id:nanoid(),
+                title:action.payload
+            }
+            state.todos.push(todo)
+        },
         removeTodo:(state, action) => {}
     }
 })
