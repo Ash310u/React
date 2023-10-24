7e70286ffda9fe6f733372bc148dd89dcc968617
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-10-24 18:40:19 +0530

    Commited updateTodo function

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -21,6 +21,10 @@ export const todoSlice = createSlice({
         },
         removeTodo:(state, action) => {
             state.todos = state.todos.filter(todo => todo.id !== action.payload)
+        },
+        updateTodo: (state,action) => {
+            const todo = state.todos.find(todo => todo.id === action.payload)
+            todo.title = action.payload
         }
     }
 })
